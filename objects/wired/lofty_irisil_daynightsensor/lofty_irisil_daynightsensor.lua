f77532ff73e7fe35e0503cde00aab4cb2601af0c
LoftyLoftyLoftyLoftyLofty <lofty@alpix.industries>
2023-07-22 19:17:43 -0500

    See Desc
    Capitalize Irisil in codices
    Add Hylotl Post Office to Hylotl ocean city dungeon
    Add Hylotl Post Office npc type
    Add Hylotl Post Office treasure pools
    Add cardboard scab critter
    Graphics for scripted switches fixed
    Graphics for daynight sensor fixed
    Update fishing eye daynight sensor to be invis
    Daynight sensor in irishop now

--- objects/wired/lofty_irisil_daynightsensor/lofty_irisil_daynightsensor.lua
+++ objects/wired/lofty_irisil_daynightsensor/lofty_irisil_daynightsensor.lua
@@ -1,4 +1,10 @@
+require "/scripts/util.lua"
+
 function init()
+	self.invisible = false
+	if config.getParameter("invisible") == true then
+		self.invisible = true
+	end
 end
 
 function update(dt)
@@ -9,9 +15,21 @@ function update(dt)
   if timeofDay <= 0.5 then
     object.setOutputNodeLevel(0, false)
 	
+	if self.invisible == false then
+		animator.setAnimationState("switchState", "day")
+	else
+		animator.setAnimationState("switchState", "invisible")
+	end
+	
   --night
   else
     object.setOutputNodeLevel(0, true)
+	
+	if self.invisible == false then
+		animator.setAnimationState("switchState", "night")
+	else
+		animator.setAnimationState("switchState", "invisible")
+	end
   end
   
 end
