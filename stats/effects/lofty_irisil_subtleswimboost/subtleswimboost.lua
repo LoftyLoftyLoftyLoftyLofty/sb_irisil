54c2efd14bf2b32ca8096a48222839e7bee882d6
LoftyLoftyLoftyLoftyLofty <lofty@alpix.industries>
2025-04-25 03:25:47 -0500

    compatibility fix for 'improved swim physics' mod

--- stats/effects/lofty_irisil_subtleswimboost/subtleswimboost.lua
+++ stats/effects/lofty_irisil_subtleswimboost/subtleswimboost.lua
@@ -3,7 +3,15 @@ function init()
 end
 
 function update(dt)
-  mcontroller.controlParameters(self.movementParameters)
+ 
+  --if the user is running the improved swim physics mod, just apply the swimboost status effect and let isp handle it
+  if status.statusProperty("ispEnabled", false) then
+    status.addEphemeralEffect("swimboost")
+	
+  --if the user is running vanilla swim physics, reduce their fluid friction and increase their movespeed
+  else
+    mcontroller.controlParameters(self.movementParameters)
+  end
 end
 
 function uninit()
